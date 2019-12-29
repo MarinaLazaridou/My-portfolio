329d23928ebdbf328e7cc12f56c071a543e4fa22
MarinaLazaridou <54821606+MarinaLazaridou@users.noreply.github.com>
2019-12-29 15:46:32 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Portfolio
+Map portfolio
+"# My-portfolio" 
---
+++ portfolio.js
@@ -0,0 +1,34 @@
+// This is an HTML5 document.
+
+
+// Author: Marina Lazaridou
+// Subject: Portfolio
+// File Format: JavaScript
+
+
+// Start script
+
+
+
+// Modal image
+
+window.onload = function() {
+	var modal = document.getElementById('myModal');
+	var images = document.getElementsByClassName('myImages');
+	var modalImg = document.getElementById("img01");
+	var captionText = document.getElementById("caption");
+
+	for (var i = 0; i < images.length; i++) {
+		images[i].onclick = function() {
+			modal.style.display = "block";
+			modalImg.src = this.src;
+			captionText.innerHTML = this.alt;
+		}
+	}
+
+	var span = document.getElementsByClassName("close")[0];
+
+	span.onclick = function() {
+		modal.style.display = "none";
+	}
+};
